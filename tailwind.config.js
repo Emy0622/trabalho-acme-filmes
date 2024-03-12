637d9005ad11a9bf30bcb58e6fda77d3764775d7
YASMIN TARGINO DE ALEXANDRE <23111771@CFP127ALUNO.LOCAL>
2024-03-12 13:51:18 -0300

    degrade

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: { 'preto': '#000000', 'vermelho': '#9A0000' }
+        },
+    },
+    plugins: [],
+}
